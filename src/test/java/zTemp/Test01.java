aa5d4b4362616c048c9252507bc4b8ee7b207dfb
bija chh <error.zxz@gmail.com>
2021-05-08 21:55:02 -0400

    added Ignore pack-test-class demo

--- src/test/java/zTemp/Test01.java
+++ src/test/java/zTemp/Test01.java
@@ -1,40 +1,46 @@
 package zTemp;
 
-import java.util.List;
 
+import java.util.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Test;
 
-import seleniumWebdriver.S01_openCloseBrowser;
-import utilites_library.Base;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class Test01 extends Base {
+public class Test01 {
 
-	@Test
-	public void pagetab() throws InterruptedException {
-		driver.get("https://learn.letskodeit.com/p/practice");
-		
-		JavascriptExecutor js = ((JavascriptExecutor)driver);
-		js.executeScript("window.scrollTo(0, 500)");
-		
-		  WebElement table = driver.findElement(By.cssSelector("table[id='product']"));
-		  List<WebElement> table_row = table.findElements(By.tagName("td"));
-		  
-				
-		for(WebElement rows :table_row) {
-			System.out.println(rows.getText());
-			if (rows.getText().equalsIgnoreCase("35")) {
-				break;
-			}
+	/*****************************************************
+	 * Docs:- https://chromedriver.chromium.org/mobile-emulation
+	 ******************************************************/
+	    @Test
+		public void ChromeMobileEmulator_withSize() {
+	    	Map<String, Object> deviceMetrics = new HashMap<>();
+	    	deviceMetrics.put("width", 360);
+	    	deviceMetrics.put("height", 640);
+	    	deviceMetrics.put("pixelRatio", 3.0);
+	    	Map<String, Object> mobileEmulation = new HashMap<>();
+	    	mobileEmulation.put("deviceMetrics", deviceMetrics);
+	    	//mobileEmulation.put("userAgent", "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19");
+	    	
+	    	mobileEmulation.put("userAgent", "Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0 Mobile/15E148 Safari/604.1");
+		    
+	    	ChromeOptions chromeOptions = new ChromeOptions(); 
+	    	chromeOptions.setExperimentalOption("mobileEmulation", mobileEmulation);
+	    	
+	    	WebDriverManager.chromedriver().setup();
+	    	WebDriver driver = new ChromeDriver(chromeOptions);
+	    	
+	    	//Verify desired device open in Browser
+			String uAgent = (String) ((JavascriptExecutor) driver).executeScript("return navigator.userAgent;"); //navigator.appVersion
+			System.out.println("==>" + uAgent);
+			System.out.println("Executed Device Name:- "+uAgent.substring(13, 37));
 			
-		}
-		
-		
-		
-		
-		
-	}
+	    	driver.get("https://www.facebook.com/");
+	    }
+
 }
