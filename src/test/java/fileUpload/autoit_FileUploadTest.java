95f521341c0ce754d1ab78188914dd67462921b8
bija chh <error.zxz@gmail.com>
2023-04-12 00:03:35 -0400

    added Complete Instruction to run on all 3 browser (Chrome,Edge,Firefox)

--- src/test/java/fileUpload/autoit_FileUploadTest.java
+++ src/test/java/fileUpload/autoit_FileUploadTest.java
@@ -8,6 +8,24 @@ import utilites_library.Base;
 
 public class autoit_FileUploadTest extends Base{
 
+	/**
+	  Setup Note: 
+	 1. Need to install Autoit3
+	 2. Go to C:\Program Files (x86)\AutoIt3\SciTE\SciTE.exe 
+	 3. Open SciTE.exe file, it will lunch editer
+	 4. Write this script in the editer and save as autoit.au3
+	 
+	 ---------For CHROME & EDGE---------- Open window by chrome & edge Window title is open that why?
+	    ControlFocus("Open", "", "Edit1")
+		ControlSetText("Open", "", "Edit1", "C:\Users\Bijaya Chhetri\eclipse-workspace\selenium-maven-java\Include\Upload file.pdf")
+		ControlClick("Open", "", "Button1")
+		
+	 ---------For FIREFOX---------- Replace with [" Open "] with [" File Upload "]
+	 Open window by firefox Window title is FIle Upload that why?
+	 
+	 4. Open the Autoit folder and Right click the autoit.au3 file ==> click Compile Script (x64)
+	 5. It will create the autoit.exe file, which we run from the script
+	 */
 	@Test
 	public void autoItFileUpload() throws InterruptedException, IOException {
 		driver.get("http://demo.automationtesting.in/FileUpload.html"); 
