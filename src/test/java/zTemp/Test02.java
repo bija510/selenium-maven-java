0da255c6e1555e1f0c6835049558f6b2891e6342
bija chh <error.zxz@gmail.com>
2025-01-14 22:41:16 -0500

    just added test case listener

--- src/test/java/zTemp/Test02.java
+++ src/test/java/zTemp/Test02.java
@@ -1,5 +1,17 @@
 package zTemp;
 
+import org.testng.TestListenerAdapter;
+import org.testng.TestNG;
+
 public class Test02 {
 
+	public static void main(String[] args) throws Exception {
+		TestListenerAdapter tla = new TestListenerAdapter();
+		TestNG testng = new TestNG();
+		testng.setTestClasses(new Class[] { Test01.class });
+		testng.addListener(tla);
+		testng.run();
+
+		
+	}
 }
